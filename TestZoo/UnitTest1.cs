30302c0beed4f6bd0035bc3d792936c1bd5f91b0
ramawm20 <ramawael54@gmail.com>
2023-07-01 15:35:28 +0300

    Add project files.

---
+++ TestZoo/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestZoo
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Zoo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Zoo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
